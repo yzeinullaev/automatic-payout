0488147e0cfa04d2bd7e03786d7cabd0dd97343b
Yernur <yzeinullayev@almanit.kz>
2022-08-01 02:21:04 +0600

    Редайрект 1 страницы

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return Redirect::to('/admin');
 });
 
 
